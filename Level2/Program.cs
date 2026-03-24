7e6c4ef895757999e0ae39a470ff68c14cc66cc0
sarah kiarie <sarahwanjiruki1@gmail.com>
2026-03-24 19:54:30 +0300

    level two task one done

--- Level2/Program.cs
+++ Level2/Program.cs
@@ -4,8 +4,13 @@ class Program
 {
     static void Main()
     { 
+    
        try
        {
+        while(true){
+         Patient? patient = null;
+        Doctor? doctor = null;
+
          Console.WriteLine("1. Register Patient \n 2. Add Doctor \n 3. Get your diagnosis \n Choose an option:");
         string? input = Console.ReadLine();
         if(input == "1")
@@ -22,7 +27,7 @@ class Program
              Console.WriteLine("what are your symptoms");
             string? symptoms = Console.ReadLine();
 
-            Patient patient = new(name, phoneNumber, age, symptoms);
+            patient = new(name!, phoneNumber, age, symptoms!);
             patient.GetDetails();
 
         }else if(input == "2")
@@ -36,16 +41,24 @@ class Program
             Console.WriteLine("enter your phoneNumber");
             int phoneNumber = Convert.ToInt32(Console.ReadLine());
 
-             Console.WriteLine("what are your symptoms");
+             Console.WriteLine("enter your specialty");
             string? specialty = Console.ReadLine();
 
-            Doctor doctor = new(name, phoneNumber, age, specialty);
-            doctor.GetDetails();
-        }else
-            {
-                
-            }
-
+            doctor = new(name!, phoneNumber, age, specialty!);
+            
+        }else if(input == "3")
+        {
+                if( patient == null || doctor == null)
+                {
+                    Console.WriteLine("please register doctor and patient first");
+                }
+                Console.WriteLine("Enter diagnosis");
+                string? diagnosis = Console.ReadLine();
+                doctor!.Diagnosis = diagnosis;
+                doctor.GetDetails();
+        }
+     if(input == "4") break;
+       }
        }
        catch (InvalidAgeException exce)
        {
